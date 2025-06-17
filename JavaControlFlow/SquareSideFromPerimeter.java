a4727ddb115d986c4195c8f340d6c06045fdac33
Dileep33249 <mdileep320@gmail.com>
2025-06-17 23:27:57 +0530

    [Dileep Add program to find side of square from perimeter]

---
+++ JavaControlFlow/SquareSideFromPerimeter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Q16_SquareSideFromPerimeter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the perimeter of the square: ");
+        double perimeter = scanner.nextDouble();
+
+        double side = perimeter / 4.0;
+
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+    }
+}
