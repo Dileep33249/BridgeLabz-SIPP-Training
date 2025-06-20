215d2aea2d3cc717aac98432d0c60a368b7231f2
Dileep33249 <mdileep320@gmail.com>
2025-06-20 09:43:04 +0530

    implement wind chill temperature calculator

---
+++ JavaMethods/WindChillCalculator.java
@@ -0,0 +1,12 @@
+public class WindChillCalculator {
+    public static double calculateWindChill(double temperature, double windSpeed) {
+        return 35.74 + 0.6215 * temperature
+                - 35.75 * Math.pow(windSpeed, 0.16)
+                + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+    }
+
+    public static void main(String[] args) {
+        double temperature = 30, windSpeed = 10; 
+        System.out.printf("Wind Chill: %.2f\n", calculateWindChill(temperature, windSpeed));
+    }
+}
