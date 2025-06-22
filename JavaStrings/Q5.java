270aea00ca098c87aae1bb58702e0ba065d48094
Dileep33249 <mdileep320@gmail.com>
2025-06-22 12:20:20 +0530

    Created substring manually using charAt and compared with built-in substring()

---
+++ JavaStrings/Q5.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Q5 {
+    public static String customSubstring(String str, int start, int end) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = start; i < end; i++) {
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public static boolean compare(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) return false;
+        }
+        return true;
+    }
