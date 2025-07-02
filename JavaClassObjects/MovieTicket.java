3595e62706737b090ef4ec37430b37ef2c43ab82
Dileep33249 <mdileep320@gmail.com>
2025-07-02 10:30:09 +0530

    Dileep Added Level 2 Program - Movie Ticket Booking System Simulation

---
+++ JavaClassObjects/MovieTicket.java
@@ -0,0 +1,23 @@
+class MovieTicket {
+    String movieName;
+    int seatNumber;
+    double price;
+
+    void bookTicket(String movieName, int seatNumber, double price) {
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.price = price;
+    }
+
+    void displayTicketDetails() {
+        System.out.println("Movie Name: " + movieName);
+        System.out.println("Seat Number: " + seatNumber);
+        System.out.println("Price: ₹" + price);
+    }
+
+    public static void main(String[] args) {
+        MovieTicket ticket = new MovieTicket();
+        ticket.bookTicket("Interstellar", 25, 300);
+        ticket.displayTicketDetails();
+    }
+}
